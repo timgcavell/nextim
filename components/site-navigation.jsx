12ab6e887f4c3654dacc97ad209a8a07fdd155bb
Tim Cavell <tim@timgcavell.com>
2023-02-09 20:20:20 -0600

    Remove twitter

--- components/site-navigation.jsx
+++ components/site-navigation.jsx
@@ -6,8 +6,7 @@ export default function SiteNavigation() {
       <Link className="nav-link" href="/">Home</Link>
       <Link className="nav-link" href="mailto:me@timgcavell.com">Email</Link>
       <Link className="nav-link" href="https://github.com/timgcavell">Code</Link>
-      <Link className="nav-link" href="https://twitter.com/timcavell">Tweets</Link>
-      <Link rel="me" href="https://hachyderm.io/@timcavell"></Link>
+      <Link className="nav-link" rel="me" href="https://hachyderm.io/@timcavell">Mastodon</Link>
     </nav>
   );
 }
