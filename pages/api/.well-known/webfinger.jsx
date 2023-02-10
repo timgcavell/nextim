8dd50838d06322075b4cb5f2e2c2413906e493d3
Tim Cavell <tim@timgcavell.com>
2023-02-09 20:55:41 -0600

    Minify webfinger result

--- pages/api/.well-known/webfinger.jsx
+++ pages/api/.well-known/webfinger.jsx
@@ -3,29 +3,5 @@ export const config = {
   };
 
   export default function handler(req, res) {
-    res.send(`{
-    "subject":"acct:timcavell@hachyderm.io",
-    "aliases":
-    [
-        "https://hachyderm.io/@timcavell",
-        "https://hachyderm.io/users/timcavell"
-    ],
-    "links":
-    [
-        {
-            "rel":"http://webfinger.net/rel/profile-page",
-            "type":"text/html",
-            "href":"https://hachyderm.io/@timcavell"
-        },
-        {
-            "rel":"self",
-            "type":"application/activity+json",
-            "href":"https://hachyderm.io/users/timcavell"
-        },
-        {
-            "rel":"http://ostatus.org/schema/1.0/subscribe",
-            "template":"https://hachyderm.io/authorize_interaction?uri={uri}"
-        }
-    ]
-}`);
+    res.send('{"subject":"acct:timcavell@hachyderm.io","aliases":["https://hachyderm.io/@timcavell","https://hachyderm.io/users/timcavell"],"links":[{"rel":"http://webfinger.net/rel/profile-page","type":"text/html","href":"https://hachyderm.io/@timcavell"},{"rel":"self","type":"application/activity+json","href":"https://hachyderm.io/users/timcavell"},{"rel":"http://ostatus.org/schema/1.0/subscribe","template":"https://hachyderm.io/authorize_interaction?uri={uri}"}]}');
 }
